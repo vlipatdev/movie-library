24eebcaa38e0f3ec6781c35f3df2a92b3f3b00cb
vlipatdev <vlipat.dev@gmail.com>
2019-07-03 22:31:13 +0800

    Add movie details

--- src/js/index.js
+++ src/js/index.js
@@ -14,7 +14,7 @@ const elements = {
     trendSelect: document.querySelector('.select__trend'),
     discoverLabel: document.querySelector('.label__discover'),
     discoverSelect: document.querySelector('.select__discover'),
-    fillerAnchor: document.querySelector('.filler-anchor'),
+    fillerAnchor: document.querySelector('.movie__filler-anchor'),
     spinner: document.querySelector('.spinner'),
 };
 
@@ -52,26 +52,46 @@ const clearMovies = () => {
     curPage = 1;
 };
 
-// axios function
-const axiosFn = (url) => {
-    console.log(url);
+let Y; // scroll y
+const movieContainer = document.querySelector('.movie__container');
+
+// get movies function
+const getMovies = (url) => {
     axios.get(url)
         .then((data) => {
-            // stop showing spinner if there's no more results
+            // stop showing spinner if no more results
             if (data.data.page >= data.data.total_pages) {
                 elements.spinner.style.display = 'none';
             }
 
+            // insert markup
             const { results } = data.data;
             results.forEach((movie) => {
                 elements.fillerAnchor.insertAdjacentHTML('beforebegin', `
-                    <article class="movie">
+                    <article data-movieid="${movie.id}" class="movie">
                         <img class="movie__poster" src="https://image.tmdb.org/t/p/w154/${movie.poster_path}">
                         <div class="movie__title-container">
                             <p class="movie__title">${movie.original_title} (${movie.release_date.slice(0, 4)})</p>
                         </div>
                     </article>
                 `);
+            });
+
+            // add click listener to each movie
+            const movieList = document.querySelectorAll('.movie');
+            movieList.forEach((movieEl) => {
+                movieEl.addEventListener('click', (e) => {
+                    Y = window.scrollY; // track scroll position
+                    e.stopImmediatePropagation();
+                    movieContainer.style.display = 'none';
+                    elements.heading.style.visibility = 'hidden';
+                    elements.discoverLabel.style.visibility = 'hidden';
+                    elements.trendSelect.style.visibility = 'hidden';
+                    elements.spinner.style.display = 'none';
+                    movieInfo.style.display = 'flex';
+                    getMovieDetails(`https://api.themoviedb.org/3/movie/${movieEl.dataset.movieid}?api_key=${key}&append_to_response=videos`);
+                    return Y;
+                });
             });
         })
         .catch(error => alert(error));
@@ -87,7 +107,7 @@ const loadNextPage = () => {
     } else if (isTrending) {
         url = `https://api.themoviedb.org/3/trending/movie/${trendValue}?api_key=${key}&page=${curPage}`;
     }
-    axiosFn(url);
+    getMovies(url);
 };
 
 // onload
@@ -102,7 +122,7 @@ const init = () => {
     discoverValue = 'popularity.desc';
     curPage = 1;
     url = `https://api.themoviedb.org/3/trending/movie/${trendValue}?api_key=${key}&page=${curPage}`;
-    axiosFn(url);
+    getMovies(url);
 
     // set intersection observer
     const observer = new IntersectionObserver(loadNextPage);
@@ -112,9 +132,10 @@ const init = () => {
 
 init();
 
-// search tab click listener
+// search tab keydown listener
 elements.searchInput.addEventListener('keydown', (e) => {
     if (e.keyCode === 13) {
+        clearDetails();
         clearMovies();
         prevSelected.classList.remove('selected');
         elements.heading.textContent = `Results for "${e.target.value}"`;
@@ -122,12 +143,13 @@ elements.searchInput.addEventListener('keydown', (e) => {
         isSearch = true;
         searchValue = e.target.value;
         url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${searchValue}&vote_count.gte=1000&page=${curPage}`;
-        axiosFn(url);
+        getMovies(url);
     }
 });
 
 // trending tab click listener
 elements.trending.addEventListener('click', () => {
+    clearDetails();
     clearMovies();
     elements.heading.textContent = 'Trending';
     elements.trendSelect.style.display = 'block';
@@ -138,7 +160,7 @@ elements.trending.addEventListener('click', () => {
     elements.trendSelect.value = 'day';
     trendValue = 'day';
     url = `https://api.themoviedb.org/3/trending/movie/${trendValue}?api_key=${key}&page=${curPage}`;
-    axiosFn(url);
+    getMovies(url);
 
     prevSelected = elements.trending;
     return trendValue;
@@ -153,6 +175,7 @@ elements.genre.addEventListener('click', () => {
 // genres click listener
 elements.genreList.forEach((genre) => {
     genre.addEventListener('click', (e) => {
+        clearDetails();
         clearMovies();
         elements.heading.textContent = e.target.textContent;
         elements.discoverLabel.style.display = 'block';
@@ -164,7 +187,7 @@ elements.genreList.forEach((genre) => {
         discoverValue = 'popularity.desc';
         genreId = e.target.dataset.id;
         url = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&with_genres=${genreId}&sort_by=${discoverValue}&vote_count.gte=1000&page=${curPage}`;
-        axiosFn(url);
+        getMovies(url);
 
         prevSelected = e.target;
         return prevSelected;
@@ -179,7 +202,7 @@ elements.trendSelect.addEventListener('change', (e) => {
     isTrending = true;
     trendValue = e.target.value;
     url = `https://api.themoviedb.org/3/trending/movie/${trendValue}?api_key=${key}&page=${curPage}`;
-    axiosFn(url);
+    getMovies(url);
 
     return trendValue;
 });
@@ -192,7 +215,96 @@ elements.discoverSelect.addEventListener('change', (e) => {
     isDiscover = true;
     discoverValue = e.target.value;
     url = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&with_genres=${genreId}&sort_by=${discoverValue}&vote_count.gte=1000&page=${curPage}`;
-    axiosFn(url);
+    getMovies(url);
 
     return discoverValue;
 });
+
+/* =========================================MOVIE DETAILS================================================ */
+
+const movieInfo = document.querySelector('.movie-info');
+// get movie details function
+const getMovieDetails = (url) => {
+    axios.get(url)
+        .then((data) => {
+            const movieData = data.data;
+            movieInfo.style.backgroundImage = `linear-gradient(to top, rgba(0, 0, 0, .9), rgba(0, 0, 0, .8)), url('https://image.tmdb.org/t/p/w780/${movieData.backdrop_path}')`;
+
+            // insert markup
+            movieInfo.insertAdjacentHTML('beforeend', `
+            <img class="movie-info__icon--close" src="img/x.svg">
+            <div class="movie-info__details">
+                <div class="movie-info__poster-container">
+                    <img class="movie-info__poster" src="https://image.tmdb.org/t/p/w200/${movieData.poster_path}">
+                    <button class="btn movie-info__btn">Add to Favorites</button>
+                </div>
+                <div>
+                    <p class="movie-info__title">${movieData.original_title}</p>
+                    <p class="movie-info__year">${movieData.release_date.slice(0, 4)}</p>
+                    <p class="movie-info__genres"></p>
+                    <div class="movie-info__stats">
+                        <img class="movie-info__icon--star"src="img/star.svg" alt="">
+                        <p class="movie-info__stat-value">${movieData.vote_average}</p>
+                        <img class="movie-info__icon--clock"src="img/clock.svg" alt="">
+                        <p class="movie-info__stat-value">${movieData.runtime} mins</p>
+                    </div>
+                    <p class="movie-info__overview">${movieData.overview}</p>
+                    <a class="btn movie-info__btn movie-info__btn--external" href="https://www.imdb.com/title/${movieData.imdb_id}/">View on IMDb</a>
+                    <a class="btn movie-info__btn movie-info__btn--external movie-info__btn--homepage" href="${movieData.homepage}">View Homepage</a>
+                </div>
+            </div>
+            <div class="movie-info__trailer">
+                <p class="movie-info__trailer-title">Trailer</p>
+                <iframe class="movie-info__trailer-video" width="560" height="315" frameborder="0" allowfullscreen></iframe>
+            </div>
+            `);
+
+            // hide homepage button if null
+            const homepageBtn = document.querySelector('.movie-info__btn--homepage');
+            if (movieData.homepage === null) {
+                homepageBtn.style.display = 'none';
+            }
+
+            // format genres
+            const movieInfoGenres = document.querySelector('.movie-info__genres');
+            const genreArr = [];
+            movieData.genres.forEach((genre) => {
+                genreArr.push(genre.name);
+            });
+            movieInfoGenres.textContent = genreArr.join(', ');
+
+            // format trailer
+            const movieInfoTrailer = document.querySelector('.movie-info__trailer-video');
+            const trailerArr = movieData.videos.results.filter(video => (video.site === 'YouTube' && video.type === 'Trailer'));
+
+            const movieTrailerContainer = document.querySelector('.movie-info__trailer');
+            if (trailerArr.length === 0) {
+                movieTrailerContainer.style.display = 'none'; // hide trailer if non-existent
+            } else {
+                const videoKey = trailerArr[0].key;
+                movieInfoTrailer.setAttribute('src', `https://www.youtube.com/embed/${videoKey}`);
+            }
+
+            // add click listener to 'close' button
+            const closeBtn = document.querySelector('.movie-info__icon--close');
+            closeBtn.addEventListener('click', () => {
+                clearDetails();
+                window.scrollTo(0, Y);
+            });
+        })
+
+        .catch(error => alert(error));
+};
+
+// clear movie details function
+const clearDetails = () => {
+    movieInfo.style.display = 'none';
+    movieInfo.innerHTML = '';
+    movieInfo.style.backgroundImage = `linear-gradient(to top, rgba(0, 0, 0, .9), rgba(0, 0, 0, .8))`;
+    elements.heading.style.visibility = 'visible';
+    elements.discoverLabel.style.visibility = 'visible';
+    elements.trendSelect.style.visibility = 'visible';
+    movieContainer.style.display = 'flex';
+    elements.spinner.style.display = 'block';
+};
+
